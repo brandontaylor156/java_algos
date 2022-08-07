194d05b1fffc9835e2f6944f834a51ce20669117
brandontaylor156 <brandon.david4@outlook.com>
2022-08-07 01:35:19 -0700

    trashed

--- arrays/RotateMatrix/RotateMatrix.java
+++ arrays/RotateMatrix/RotateMatrix.java
@@ -9,14 +9,20 @@ public class RotateMatrix{
     }
 
     public int[][] rotateMatrix(int[][] matrix){
+        // Set pointer on all four corners
         int left, right, top, bottom; 
         left = top = 0;
         right = bottom = matrix.length-1;
         
+        // Offset for an individual layer's rotation
         int offset = 0;
 
+        // Nested for loop, outer loop closes in corner pointers by 1 
+        // Inner loop for all rotations of given layer
         while(left < right){
             offset = 0;
+            // This while condition ensures the correct number of rotations for a given layer
+            // An NxN matrix would N-1 rotations! NOT N
             while (offset < right - left){
                 int temp = matrix[top][left+offset];
 
