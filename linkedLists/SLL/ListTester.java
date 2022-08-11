e5e586b2fb264b0b3270be3e0c44924ce375cb2d
brandontaylor156 <brandon.david4@outlook.com>
2022-08-11 03:11:46 -0700

    trying to reverse SLL

--- linkedLists/SLL/ListTester.java
+++ linkedLists/SLL/ListTester.java
@@ -1,13 +1,17 @@
 public class ListTester{
     public static void main(String[] args){
         SinglyLinkedList newList = new SinglyLinkedList();
-        newList.addToFront(4);
-        newList.addToFront(6);
-        newList.addToFront(9);
-        newList.addToBack(10);
-        newList.addAtIndex(99, 4);
-        newList.deleteFromFront();
-        newList.deleteFromBack();
+        newList.addToFront(14);
+        newList.addToBack(5);
+        newList.addToBack(62);
+        newList.addToBack(0);
+        newList.addToBack(1);
+        newList.addToBack(25);
+        newList.addToBack(2);
+        newList.addToBack(58);
+        
+
+        newList.reverseList();
 
         newList.printList();
     }
--- linkedLists/SLL/SinglyLinkedList.java
+++ linkedLists/SLL/SinglyLinkedList.java
@@ -32,21 +32,14 @@ public class SinglyLinkedList{
     }
 
     // Add node at a specified index
-    // Check for invalid input (less than 0), then if list is empty, then if index is 0, then if index is greater than 0. If index is more than length of list, append to end. Anything greater, index out of range. 
+    // Check if list is empty, then if index is 0, then if index is greater than 0. If index is 1 more than length of list, append to end. Anything greater, index out of range. 
 
     // Set a previous node equal to head and runner equal to head.next
     // Start at position 1 and loop while curr != null
     // If index equals location, insert and return. After while loop, check same conditions for last node in list
     public void addAtIndex(int value, int index){
         Node newNode = new Node(value);
-        // Input validation
-        if (index < 0){
-            System.out.println("Index must be at least 0");
-            return;
-        }
-
-        // If list is empty
-        else if (head == null){
+        if (head == null){
             head = newNode;
             return;
         }
@@ -98,6 +91,143 @@ public class SinglyLinkedList{
                 runner = runner.next;
             }
             runner.next = null;
+        }
+    }
+
+    // Check empty list or list size 1 first, then if index is 0, then prev=head curr=head.next and iterate with start position 1 (while curr.next != null), then check last element (reason for curr.next). Else, out of range.
+    public void deleteAtIndex(int index){
+        if (head == null || head.next == null){
+            head = null;
+            return;
+        }
+
+        else if (index == 0){
+            head = head.next;
+        }
+
+        else {
+            Node prev = head;
+            Node curr = head.next;
+            int position = 1;
+            while (curr.next != null){
+                if (position == index){
+                    prev.next = curr.next;
+                    return;
+                }
+                position++;
+                curr = curr.next;
+                prev = prev.next;
+            }
+            if (position == index)
+                prev.next = null;
+            else
+                System.out.println("Index out of range");
+        }
+    }
+
+    // Check if head / head.next null first, else set min to head.value with prev and curr pointers, find min for all values - 1, then last value, start comparing min with last value first, then first value, then everything in the middle with another while loop
+    public void deleteMin(){
+        if (head == null || head.next == null){
+            head = null;
+            return;
+        }
+
+        else {
+            int min = head.value;
+            Node prev = head;
+            Node curr = head.next;
+            while(curr.next != null){
+                if(curr.value < min){
+                    min = curr.value;
+                }
+                prev = prev.next;
+                curr = curr.next;
+            }
+            if(curr.value < min){
+                prev.next = null;
+                return;
+            }
+
+            if(head.value == min){
+                head = head.next;
+                return;
+            }
+
+            else {
+                Node prev2 = head;
+                Node curr2 = head.next;
+                while(curr2.next != null){
+                    if (curr2.value == min){
+                        prev2.next = curr2.next;
+                        return;
+                    }
+                    prev2 = prev2.next;
+                    curr2 = curr2.next;
+                }
+            }
+        
+        }
+    }
+
+    public void moveMinToFront(){
+        if (head == null || head.next == null){
+            head = null;
+            return;
+        }
+
+        else {
+            int min = head.value;
+            Node prev = head;
+            Node curr = head.next;
+            while(curr.next!=null){
+                if (curr.value < min){
+                    min = curr.value;
+                }
+                prev = prev.next;
+                curr = curr.next;
+            }
+            if (curr.value < min){
+                curr.next = head;
+                prev.next = null;
+                head = curr;
+                return;
+            }
+
+            if (head.value == min)
+                return;
+            else {
+                Node prev2 = head;
+                Node curr2 = head.next;
+                while(curr2.next != null){
+                    if (curr2.value == min){
+                        Node temp = curr2;
+                        prev2.next = curr2.next;
+                        temp.next = head;
+                        head = temp;
+                        return;
+                    }
+                    prev2 = prev2.next;
+                    curr2 = curr2.next;
+                }
+            }
+        }
+    }
+
+    // Unfinished
+    public void reverseList(){
+        if (head == null || head.next == null){
+            return;
+        }
+        else {
+            Node prev = head;
+            Node curr = head.next;
+            while(curr != null){
+                Node temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+                      
         }
     }
     
